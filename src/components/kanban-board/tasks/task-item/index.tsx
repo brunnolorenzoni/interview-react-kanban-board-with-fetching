5a54f4e8d3d12ad4f9e7c53c14b23bb4d6192ebb
Brunno Lorenzoni <lorenzoni.brunno@gmail.com>
2024-03-23 17:52:04 +0000

    feat: add task actions

--- src/components/kanban-board/tasks/task-item/index.tsx
+++ src/components/kanban-board/tasks/task-item/index.tsx
@@ -1,10 +1,59 @@
-import { Task as TaskModel } from "@models/task";
+import { Task } from "@models/task";
+import TaskRepository from "@/repositories/task-repository";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useNotifications from "@/hooks/useNotifications";
+import { STAGE_NAMES } from "@/constants/task";
 
 type Props = {
-  task: TaskModel;
+  task: Task;
 };
 
+const repository = new TaskRepository();
+
 export default function TaskItem({ task }: Props) {
+  const notification = useNotifications(repository.resource);
+  const queryClient = useQueryClient();
+
+  const { mutate: moveTask, isPending: isMoving } = useMutation({
+    mutationFn: (data: Task) => repository.update(task.name, data),
+    onSuccess: (_) => {
+      notification.success("Task moved");
+      queryClient.refetchQueries({
+        queryKey: [repository.resource, "getMany"],
+      });
+    },
+    onError: () => {
+      notification.error("Try again later");
+    },
+  });
+
+  const { mutate: deleteTask, isPending: isDeleting } = useMutation({
+    mutationFn: () => repository.delete(task.name),
+    onSuccess: (_) => {
+      notification.success("Task removed");
+      queryClient.refetchQueries({
+        queryKey: [repository.resource, "getMany"],
+      });
+    },
+    onError: () => {
+      notification.error("Try again later");
+    },
+  });
+
+  const isStageValid = (stage: number) => {
+    return stage >= 0 && stage <= STAGE_NAMES.length;
+  };
+
+  const handleMove = (movement: number) => {
+    const stage = task.stage + movement;
+    if (!isStageValid(stage)) return;
+
+    const update = { stage } as Task;
+    moveTask(update);
+  };
+
+  const isDisabled = isMoving || isDeleting;
+
   return (
     <div className="li-content layout-row justify-content-between align-items-center">
       <span data-testid={`${task.name.split(" ").join("-")}-name`}>
@@ -14,18 +63,24 @@ export default function TaskItem({ task }: Props) {
         <button
           className="icon-only x-small mx-2"
           data-testid={`${task.name.split(" ").join("-")}-back`}
+          onClick={() => handleMove(-1)}
+          disabled={isDisabled}
         >
           <i className="material-icons">arrow_back</i>
         </button>
         <button
           className="icon-only x-small mx-2"
           data-testid={`${task.name.split(" ").join("-")}-forward`}
+          onClick={() => handleMove(1)}
+          disabled={isDisabled}
         >
           <i className="material-icons">arrow_forward</i>
         </button>
         <button
           className="icon-only danger x-small mx-2"
           data-testid={`${task.name.split(" ").join("-")}-delete`}
+          onClick={() => deleteTask()}
+          disabled={isDisabled}
         >
           <i className="material-icons">delete</i>
         </button>
